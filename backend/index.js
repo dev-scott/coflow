59dc6ce22bce5e1925d6aeb3d3355aaab4c52283
Dev-scott <sadoscott25@gmail.com>
2026-09-08 07:00:15 +0100

    fix:add new cors url

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,7 @@ app.use(
         "http://localhost:5173",
         "http://localhost:3000",
         "https://coflow.dev-scott.me",
+        "https://coflow.dev-scott.me",
         "https://coflow.dev-scott.me/sign-up"
       ].filter(Boolean);
       if (!origin || allowed.includes(origin)) {
